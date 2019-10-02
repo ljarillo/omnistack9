6f6b7c6466395a02607118ed2152e18480a25dbe
Luis Augusto <luis@neotix.com.br>
2019-10-02 11:25:45 -0300

    criado mais alguns end points

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const SpotSchema = new mongoose.Schema({
-    thumbnail: String;
+    thumbnail: String,
     company: String,
     price: Number,
     techs: [String],
